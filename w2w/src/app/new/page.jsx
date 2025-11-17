0ee67ae7823e210575f50ca8aa5d9f24f32678b8
dpm227 <dpm227@lehigh.edu>
2025-11-17 18:31:13 -0500

    created new w2w next.js app that has tailwind css, created navbar and all the pages, updated gitignore

---
+++ w2w/src/app/new/page.jsx
@@ -0,0 +1,9 @@
+import Navbar from "../navbar";
+
+export default function New() {
+  return (
+    <>
+      <Navbar />
+    </>
+  );
+}
---
+++ w2w/src/app/search/page.jsx
@@ -0,0 +1,9 @@
+import Navbar from "../navbar";
+
+export default function Search() {
+  return (
+    <>
+      <Navbar />
+    </>
+  );
+}
